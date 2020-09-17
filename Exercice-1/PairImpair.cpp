034d871760dfed39a0abb22d3572563994299117
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-17 10:15:28 -0400

    Très bon travail
    Excellent code très bien commenté. Complète le plan de tests.

--- Exercice-1/PairImpair.cpp
+++ Exercice-1/PairImpair.cpp
@@ -1,28 +1,28 @@
-//But : Écrire un programme qui lit un entier et qui détermine et affiche s'il s'agit d'un entier impair. 
+//But : Ã‰crire un programme qui lit un entier et qui dÃ©termine et affiche s'il s'agit d'un entier impair. 
 //Date : 2020-09-13
 //Nom : Marcus Asfaw
 
 
-#include <iostream>; //Pour inclure la bibliothèque qui nous permet d'utiliser Cin, Cout...
+#include <iostream>; //Pour inclure la bibliothÃ¨que qui nous permet d'utiliser Cin, Cout...
 
-using namespace std; // Pour pas être obliger de mettre std:: partout
+using namespace std; // FR : Pour pas Ãªtre obliger de mettre std:: partout
 
-int main() //La porte d'entrée du programme 
+int main() //La porte d'entrÃ©e du programme 
 {
 
 
-	setlocale(LC_ALL, "");	//Permet de bien voir les caractère accentués
+	setlocale(LC_ALL, "");	// FR : Permet de bien voir les caractÃ¨re accentuÃ©s
 
 
-	int nombre1; //Défénir le premier chiffre que l'utilisateur doit rentrer 
+	int nombre1; //FR : DÃ©fÃ©nir le premier chiffre que l'utilisateur doit rentrer 
 
 
 
-	cout << "Bonjour! Veuillez entrer un nombre entier: "; // Le premier cout la première chose que l'utilisateur voit qui lui indique
+	cout << "Bonjour! Veuillez entrer un nombre entier: "; // Le premier cout la premiÃ¨re chose que l'utilisateur voit qui lui indique
 	//															d'entrer un nombre entier
 	cin >> nombre1; //Ici on indique que notre variable nombre1 donne le chiffre que l'utilisateure entre
 
-	if (nombre1%2==0) //Ici on a la formule pour trouver si est chiffre est pair ou non le chiffre modulo 2 doit égaler a 0 pour être pair
+	if (nombre1%2==0) //Ici on a la formule pour trouver si est chiffre est pair ou non le chiffre modulo 2 doit Ã©galer a 0 pour Ãªtre pair
 	{
 		cout << nombre1 << " est un chiffre pair !"; // Si c'est le cas on lui affiche ce cout qui lui indique que le chiffre entrer est en effet pair
 
@@ -35,9 +35,9 @@ int main() //La porte d'entrée du programme
 
 	return 0;
 }
+//KM : Manque des valeurs nÃ©gatives dans le plan de tests et aussi 0 !
 
-
-/*  chiffre				Pair ou Impair				Résulat							Vrai ou Faux
+/*  chiffre				Pair ou Impair				RÃ©sulat							Vrai ou Faux
 	51					Impair						est un chiffre impair !			Vrai
 	12					Pair						est un chiffre pair !			Vrai
 	32					Pair						est un chiffre pair !			Vrai
