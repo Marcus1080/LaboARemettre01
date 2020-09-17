3afeb236b47782bc3a6d371cf63a3cb45f742241
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-17 10:18:45 -0400

    Excellent travail !
    Super, car tu as utilisé juste les notions vues en classe !!

--- Exercice-02/Vertical.cpp
+++ Exercice-02/Vertical.cpp
@@ -1,27 +1,27 @@
-//But : Écrire un programme qui prend comme un entrée un nombre de cinq chiffres le programme vérifie qu'il a bien cinq chiffres
-				//dans le nombre. Puis il affiche le nombre verticalement le premier chiffre sur la première ligne,
-//				le deuxième sur la deuxième ligne ainsi de suite
+//But : Ã‰crire un programme qui prend comme un entrÃ©e un nombre de cinq chiffres le programme vÃ©rifie qu'il a bien cinq chiffres
+				//dans le nombre. Puis il affiche le nombre verticalement le premier chiffre sur la premiÃ¨re ligne,
+//				le deuxiÃ¨me sur la deuxiÃ¨me ligne ainsi de suite
 //Date : 2020-09-13
 //Auteur : Marcus Asfaw
 
-#include <iostream>;		//Pour inclure la bibliothèque qui nous permet d'utiliser les cout et les cin ...
+#include <iostream>;		//Pour inclure la bibliothÃ¨que qui nous permet d'utiliser les cout et les cin ...
 
-using namespace std;	//Pour pas être obliger d'écrire std:: a chaque cout et cin ...
+using namespace std;	//FR : Pour pas Ãªtre obliger d'Ã©crire std:: a chaque cout et cin ...
 
-int main() //La porte d'entrée du programme 
+int main() //La porte d'entrÃ©e du programme 
 {
 
 
-	setlocale(LC_ALL, ""); //Permet de bien voir les caractères accentués
+	setlocale(LC_ALL, ""); //Permet de bien voir les caractÃ¨res accentuÃ©s
 
 	 
 
-	int nombre2; //Défénir la variable pour le nombre  a cinq chiffres
+	int nombre2; //FR : DÃ©fÃ©nir la variable pour le nombre  a cinq chiffres
 
 	
 
-	cout << "Veuillez entrer un nombre a cinq chiffres:  "; //Le premier message que lÈutilisateur voit a l'écran
-	cin >> nombre2; //La variable qui va être le nombre a cinq chiffres
+	cout << "Veuillez entrer un nombre a cinq chiffres:  "; //FR : Le premier message que lÃˆutilisateur voit a l'Ã©cran
+	cin >> nombre2; //La variable qui va Ãªtre le nombre a cinq chiffres
 
 
 	if (nombre2 > 99999)//Ici on indique que si le no,mbre a plus de cinq chiffre 
@@ -29,24 +29,25 @@ int main() //La porte d'entrée du programme
 		cout << nombre2 << " ce nombre ne contient pas cinq chiffres. " << endl;//On affiche ce message 
 		system("PAUSE");//Prend une pause pour que l'utilisateur puisse lire le message
 	}
+	// KM : Il manque un else ici
 	if (nombre2 < 10000) // Ici on indique que si l'utilisateur entre un nombre a moins de cinq chiffres
 	{
-		cout << nombre2 << " Ce nombre ne contient pas cinq chiffres. " << endl; // On affiche alors ce message a l'écran
+		cout << nombre2 << " Ce nombre ne contient pas cinq chiffres. " << endl; // On affiche alors ce message a l'Ã©cran
 		system("PAUSE");
 	} 
 	else //Sinon ....
 	{
 	 
-		cout <<" 1. " << nombre2 / 10000<< endl; //On affiche les chiffres 1 a 5 pour tout numérotés
-		nombre2 = nombre2 % 10000; // On rajoute ceci pour pas voir les chiffres avant le numéro qu'on veux
+		cout <<" 1. " << nombre2 / 10000<< endl; // FR : On affiche les chiffres 1 a 5 pour tout numÃ©rotÃ©s
+		nombre2 = nombre2 % 10000; // FR : On rajoute ceci pour pas voir les chiffres avant le numÃ©ro qu'on veux
 		cout <<" 2. " << nombre2 / 1000 << endl;//On descend en allant de 10000 jusqu'a 1
-		nombre2 = nombre2 % 1000; // Répète la même commande pour chaque ligne mais en enlevant un 0 a chaque fois
+		nombre2 = nombre2 % 1000; // RÃ©pÃ¨te la mÃªme commande pour chaque ligne mais en enlevant un 0 a chaque fois
 		cout <<" 3. " << nombre2 / 100 << endl;// 100..
 		nombre2 = nombre2 % 100; // descend encore
 		cout << " 4. " << nombre2 / 10 << endl; //10..
 		nombre2 = nombre2 % 10; // descend encore
 		cout << " 5. " << nombre2 / 1 << endl; //Jusqu'au dernier qui est 1 
-		nombre2 = nombre2 % 1; // et la même commande une dernière fois
+		nombre2 = nombre2 % 1; // et la mÃªme commande une derniÃ¨re fois
 
 	}
 
@@ -56,7 +57,7 @@ int main() //La porte d'entrée du programme
 
 
 
-/*	Chiffre :				Résultat qu'on devrait obtenir :						Résulat obtenue	:					Vrai/Faux :
+/*	Chiffre :				RÃ©sultat qu'on devrait obtenir :						RÃ©sulat obtenue	:					Vrai/Faux :
 
 	12345					1. 1, 2. 2, 3. 3, 4. 4, 5. 5							1. 1, 2. 2, 3. 3, 4. 4, 5. 5			Vrai
 							
